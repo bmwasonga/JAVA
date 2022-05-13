1c3192974eb36fc47e728768bb4a043c7d133b96
bmwasonga <bmwasonga@gmail.com>
2022-05-13 21:09:38 +0300

    Hello Java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+  public static void main (String[] args) {
+    Sttring hello = "Hello World!";
+    System.out.println(hello);
+  }
+  
+  public static String sayHi() {
+    return "Hi!";
+  }
+  }
