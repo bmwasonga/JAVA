fad5d7b93b9a31f0531a88e3446274b18f65074e
bmwasonga <bmwasonga@gmail.com>
2022-05-14 02:57:43 +0300

    java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 class HelloWorld {
-  public static void main (String[] args) {
-    Sttring hello = "Hello World!";
+  public static void main(String[] args) {
+    String hello = "Hello World!";
     System.out.println(hello);
   }
-  
+
   public static String sayHi() {
     return "Hi!";
   }
-  }
+}
